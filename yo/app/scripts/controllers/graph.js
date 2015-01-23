66d3b20da35a752fdb281acd460e8bf2594900ad
Thibaud Kehler <Thibaud.Kehler@gmx.net>
2015-01-23 14:04:24 +0100

    added Editorfunctions

--- yo/app/scripts/controllers/graph.js
+++ yo/app/scripts/controllers/graph.js
@@ -11,8 +11,33 @@
 
 angular.module('graphOfContentApp')
 .controller('GraphCtrl', function($scope, $http, graphURL, graphRenderer){
-	$http.get(graphURL+'/1').then(function(response) {
+	/*$http.get(graphURL+'/1').then(function(response) {
 		graphRenderer.setGraphData(response.data);
-	});
+	});*/
+	var g = {
+		      nodes: [],
+		      edges: []
+		    };
+
+	graphRenderer.setGraphData(g);
+	
+	$scope.addNode = function(){
+		$scope.nodeTransfer = {
+			id: 'm'+Math.random(),
+			label: $scope.nodeLabel,
+			x: $scope.nodeXPos,
+			y: $scope.nodeYPos,
+			type: $scope.nodeType,
+			size: 1
+		};
+		
+		g.nodes.push($scope.nodeTransfer);
+		graphRenderer.addNode($scope.nodeTransfer);
+		
+	};
+	
+	
 	
 });
+
+	
